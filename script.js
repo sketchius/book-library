add61f0f04999366bb52eaf5f47bf7e8c39bbf5c
Bryce Huhtala <sketchius@gmail.com>
2022-04-20 19:22:16 -0400

    Add delete book and mark read buttons, add layout improvements

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ const bookFormCancelButton = document.querySelector('#bookFormCancelButton');
 
 const bookDisplayContainer = document.querySelector('.content');
 
+let maxTitleCharacters = 0;
+
 newBookButton.addEventListener('click',showNewBookForm);
 bookFormCancelButton.addEventListener('click',hideNewBookForm);
+bookFormAddButton.addEventListener('click',addBookFromForm);
 
-testBook = new Book('Ender\'s Game','Orson Scott Card','1985','324 p.','4.5/5');
+testBook = new Book('Ender\'s Game','Orson Scott Card','1985','324','4.5');
 addBookToLibrary(testBook);
 
 function Book(title, author, date, pages, rating) {
@@ -38,17 +41,22 @@ function addBookToLibrary(book) {
   deleteIconElement.classList.add('remove');
   deleteIconElement.setAttribute('src','assets/remove.png');
   deleteIconElement.setAttribute('alt','Remove Icon');
+  deleteIconElement.addEventListener('click',function ()   { bookDisplayContainer.removeChild(bookCardElement); } );
   titleAndRemoveContainerElement.appendChild(titleElement);
   titleAndRemoveContainerElement.appendChild(deleteIconElement);
+
+
+  const bookCardBottomSection = document.createElement('div');
+  bookCardBottomSection.classList.add('bookCardBottomSection');
 
   const authorAndPagesContainerElement = document.createElement('div');
   authorAndPagesContainerElement.classList.add('author-and-pages');
   const authorElement = document.createElement('div');
   authorElement.classList.add('author');
-  authorElement.textContent = book.author;
+  authorElement.textContent = 'by ' + book.author;
   const pagesElement = document.createElement('div');
   pagesElement.classList.add('pages');
-  pagesElement.textContent = book.pages;
+  pagesElement.textContent = book.pages + ' pages';
   authorAndPagesContainerElement.appendChild(authorElement);
   authorAndPagesContainerElement.appendChild(pagesElement);
 
@@ -59,30 +67,55 @@ function addBookToLibrary(book) {
   dateElement.textContent = book.date;
   const ratingElement = document.createElement('div');
   ratingElement.classList.add('rating');
-  ratingElement.textContent = book.rating;
+  ratingElement.textContent = book.rating + '/5';
   dateAndRatingContainerElement.appendChild(dateElement);
   dateAndRatingContainerElement.appendChild(ratingElement);
 
+
+ 
+
   const buttonsContainerElement = document.createElement('div');
   buttonsContainerElement.classList.add('buttons');
+  const readElement = document.createElement('div')
+  readElement.classList.add('readText');
+  readElement.textContent = 'NOT READ';
   const dummyDiv = document.createElement('div');
+  const markReadIcon = document.createElement('img');
+  markReadIcon.setAttribute('src','assets/read.png');
+  markReadIcon.setAttribute('alt','Mark Read Icon');
+
+  markReadIcon.addEventListener('click',function () { markRead(book) });
+
   const favoriteIconElement = document.createElement('img');
   favoriteIconElement.setAttribute('src','assets/favorite.png');
   favoriteIconElement.setAttribute('alt','Favorite Icon');
   const shareIconElement = document.createElement('img');
   shareIconElement.setAttribute('src','assets/share.png');
   shareIconElement.setAttribute('alt','Share Icon');
+  buttonsContainerElement.appendChild(readElement);
   buttonsContainerElement.appendChild(dummyDiv);
+  buttonsContainerElement.appendChild(markReadIcon);
   buttonsContainerElement.appendChild(favoriteIconElement);
   buttonsContainerElement.appendChild(shareIconElement);
 
+  bookCardBottomSection.appendChild(authorAndPagesContainerElement);
+  bookCardBottomSection.appendChild(dateAndRatingContainerElement);
+  bookCardBottomSection.appendChild(buttonsContainerElement);
+
+
+
 
   bookCardElement.appendChild(titleAndRemoveContainerElement);
-  bookCardElement.appendChild(authorAndPagesContainerElement);
-  bookCardElement.appendChild(dateAndRatingContainerElement);
-  bookCardElement.appendChild(buttonsContainerElement);
+  bookCardElement.appendChild(bookCardBottomSection);
 
   bookDisplayContainer.appendChild(bookCardElement);
+
+  updateContentColumnsize(book.title);
+
+  book.htmlObject = bookCardElement;
+  book.readText = readElement;
+  book.readIcon = markReadIcon;
+  myLibrary.push(book);
 }
 
 
@@ -96,18 +129,55 @@ function showNewBookForm() {
 
 function startNewBookFormTransition() {
   newBookForm.style.top = '0';
+  newBookForm.style.width = '30ch';
   newBookForm.style.opacity = '100%';
 }
 
 
 function hideNewBookForm() {
+  const titleInput = document.querySelector('#addbook-title');
+  const authorInput = document.querySelector('#addbook-author');
+  const pagesInput = document.querySelector('#addbook-pages');
+  const dateInput = document.querySelector('#addbook-date');
+  const ratingInput = document.querySelector('#addbook-rating');
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  dateInput.value = '';
+  ratingInput.value = '';
   newBookForm.style.top = '-500px';
+  newBookForm.style.width = '18ch';
   newBookForm.style.opacity = '0%';
-  window.setTimeout( showSidebarButtons, 500);
+  window.setTimeout( showSidebarButtons, 250);
 }
 
 function showSidebarButtons() {
   newBookFormContainer.style.display = 'none';
   newBookButton.style.display = 'grid';
   sortButton.style.display = 'grid';
+}
+
+function addBookFromForm() {
+  const titleInput = document.querySelector('#addbook-title');
+  const authorInput = document.querySelector('#addbook-author');
+  const pagesInput = document.querySelector('#addbook-pages');
+  const dateInput = document.querySelector('#addbook-date');
+  const ratingInput = document.querySelector('#addbook-rating');
+  const newBook = new Book(titleInput.value,authorInput.value,dateInput.value,pagesInput.value,ratingInput.value);
+  addBookToLibrary(newBook);
+  hideNewBookForm();
+}
+
+function updateContentColumnsize(newTitle) {
+  if (false && newTitle.length > maxTitleCharacters) {
+    maxTitleCharacters = Math.min(newTitle.length,25);
+    const content = document.querySelector('.content');
+    content.style.gridTemplateColumns = 'repeat(auto-fit,minmax(' + (maxTitleCharacters*1.8) + 'ch,' + ((maxTitleCharacters+3)*1.8) + 'ch))';
+  }
+}
+
+function markRead(book) {
+  book.read = true;
+  book.readText.textContent = '✓ READ';
+  book.readIcon.style.opacity = 0;
 }
